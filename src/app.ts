bca6cbaeee9e7fe5f81998d6382267c10405a134
aster <137767097+aster-void@users.noreply.github.com>
2024-11-20 20:33:04 +0900

    ok

--- src/app.ts
+++ src/app.ts
@@ -22,25 +22,34 @@ const resForHuman = `
 	</p>
 </center>`;
 
+async function main(req: Request, env: Env) {
+	if (req.method !== "POST") {
+		throw null;
+	}
+	const body = v.parse(Body, await req.json());
+	if (
+		body.key !==
+		"2e0c7cad39e09314a46f217c6107f96e08bd13984cd4ae4c29d96f5db440dba8"
+	) {
+		return new Response("key validation failed");
+	}
+	const db = drizzle(env.DB);
+	await db.insert(visitsTable).values({
+		url: body.url,
+		at: new Date(),
+	});
+	return new Response("ok");
+}
+
 const bareapp = {
 	async fetch(req: Request, env: Env, _ctx: unknown): Promise<Response> {
-		if (req.method !== "POST") {
+		try {
+			return await main(req, env);
+		} catch (err) {
 			const res = new Response(resForHuman);
 			res.headers.append("Content-Type", "text/html");
 			return res;
 		}
-		const body = v.parse(Body, await req.json());
-		if (
-			body.key !==
-			"2e0c7cad39e09314a46f217c6107f96e08bd13984cd4ae4c29d96f5db440dba8"
-		)
-			return new Response("key validation failed");
-		const db = drizzle(env.DB);
-		await db.insert(visitsTable).values({
-			url: body.url,
-			at: new Date(),
-		});
-		return new Response("ok");
 	},
 };
 
