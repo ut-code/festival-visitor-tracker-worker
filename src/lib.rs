2f85fe7bbd39790ba22adbd2c8dd819ef292d804
aster <137767097+aster-void@users.noreply.github.com>
2024-11-20 16:01:39 +0900

    reset: let's just do it in js

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-use worker::{event, Context, Env, HttpRequest};
-
-#[event(fetch)]
-async fn fetch(_req: HttpRequest, _env: Env, _ctx: Context) -> worker::Result<worker::Response> {
-    console_error_panic_hook::set_once();
-    let res = worker::Response::ok("Hello World!");
-    Ok(res?)
-}
