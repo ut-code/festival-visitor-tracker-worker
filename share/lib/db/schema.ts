bf469d1a1e5b8b53ab18583afd9e68b043b40932
aster <137767097+aster-void@users.noreply.github.com>
2024-11-21 00:54:26 +0900

    separate drizzle into share

--- share/lib/db/schema.ts
+++ share/lib/db/schema.ts
@@ -1,7 +1,7 @@
 import { int, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const visitsTable = sqliteTable("visits", {
-	id: int().primaryKey({ autoIncrement: true }),
-	url: text().notNull(),
-	at: integer({ mode: "timestamp" }).notNull(),
+  id: int().primaryKey({ autoIncrement: true }),
+  url: text().notNull(),
+  at: integer({ mode: "timestamp" }).notNull(),
 });
--- worker/drizzle.config.ts
+++ worker/drizzle.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "drizzle-kit";
 import { unwrap } from "./src/utils";
 
 export default defineConfig({
-	out: "./drizzle",
-	schema: "./src/db/schema.ts",
-	dialect: "sqlite",
-	driver: "d1-http",
-	dbCredentials: {
-		accountId: unwrap(process.env.CLOUDFLARE_ACCOUNT_ID),
-		databaseId: unwrap(process.env.CLOUDFLARE_DATABASE_ID),
-		token: unwrap(process.env.CLOUDFLARE_D1_TOKEN),
-	},
+  out: "./drizzle",
+  schema: "../share/lib/db/schema.ts",
+  dialect: "sqlite",
+  driver: "d1-http",
+  dbCredentials: {
+    accountId: unwrap(process.env.CLOUDFLARE_ACCOUNT_ID),
+    databaseId: unwrap(process.env.CLOUDFLARE_DATABASE_ID),
+    token: unwrap(process.env.CLOUDFLARE_D1_TOKEN),
+  },
 });
