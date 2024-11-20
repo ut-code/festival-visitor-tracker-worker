78c5c488993bd982d1aa1a5076480c326baa491d
aster <137767097+aster-void@users.noreply.github.com>
2024-11-20 22:44:52 +0900

    monorepo: move everything from other repo to view/

---
+++ view/tailwind.config.ts
@@ -0,0 +1,16 @@
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import containerQueries from "@tailwindcss/container-queries";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
+
+export default {
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+
+	theme: {
+		extend: {},
+	},
+
+	plugins: [typography, forms, containerQueries, aspectRatio, daisyui],
+} satisfies Config;
