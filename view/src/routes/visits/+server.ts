56df13f978e13ce9009c813bea6c9794f7802cc6
aster <137767097+aster-void@users.noreply.github.com>
2024-11-22 13:19:52 +0900

    fix param parsing

--- view/src/routes/visits/+server.ts
+++ view/src/routes/visits/+server.ts
@@ -3,11 +3,14 @@ import { and, eq, gte } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/d1';
 import { visitsTable } from '~/db/schema';
 
-export const GET: ServerLoad = async ({ params, platform }) => {
-	const duration = Number.parseInt(params.duration ?? '');
-	if (!duration) return new Response(`failed to parse ${params.duration} to number`);
+export const GET: ServerLoad = async ({ url, platform }) => {
+	const duration = Number.parseInt(url.searchParams.get('duration') ?? '');
+	if (!duration)
+		return new Response(
+			`{"error": "failed to parse ${url.searchParams.get('duration')} to number"}`
+		);
 	const threshold = new Date(new Date().getTime() - duration);
-	const kind = params.kind ?? 'all';
+	const kind = url.searchParams.get('kind') ?? 'all';
 
 	if (!platform) return new Response('platform not found');
 	const db = drizzle(platform.env.DB);
