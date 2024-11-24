d448e03f850e799dd655e2a0149f3b21a210b410
aster <137767097+aster-void@users.noreply.github.com>
2024-11-24 18:04:06 +0900

    all getTime is optional now

--- view/src/routes/+page.svelte
+++ view/src/routes/+page.svelte
@@ -18,7 +18,7 @@
 	const now = new Date();
 	const today = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
 	let last: Date = $state(today);
-	let start: Date = $state(new Date(today.getTime() - 1 * DAY));
+	let start: Date = $state(new Date(today?.getTime() - 1 * DAY));
 </script>
 
 <header>
